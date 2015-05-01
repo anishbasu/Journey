3c00e5f82d071714d7db3408308453a8e53a46f7
Anish Basu <anish@drexel.edu>
2015-05-01 23:39:33 +0530

    Now you can actually enter posts!

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
-angular.module('journey', ['contenteditable'])
-  .controller('mainCtrl', ['$scope', function($scope) {
+var journey = angular.module('journey', ['contenteditable','ngSanitize'])
+
+journey.controller('mainCtrl', ['$scope', function($scope) {
     $scope.txt="Type some stuff out!"
-  }])
+    $scope.posts_n = 0//localStorage.getItem("posts_n")!=null?localStorage.getItem("posts_n"):0;
+    $scope.posts = new Array()//localStorage.getItem("post")!=null?JSON.parse(localStorage.get("post")):new Array();
+    $scope.submitPost = function () {
+      sub_post = {id: $scope.posts_n, date: new Date().toDateString(), txt:$scope.txt}
+      $scope.posts.push(sub_post)
+      $scope.posts_n = $scope.posts_n++;
+      $scope.txt = "";
+    }
+
+    
+}])
